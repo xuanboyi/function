6ae2b609392436d89f37e12a989827a16eb0127d
玄博屹 <34412317+xuanboyi@users.noreply.github.com>
2017-12-12 11:32:31 +0800

    Update Person.h

--- class1/Person.h
+++ class1/Person.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 
 @interface Person : NSObject
-@property(nonatomic,assign)NSString *name ;
+@property(nonatomic,copy)NSString *name ;
 @property(nonatomic,assign)int age ;
 @property(nonatomic,assign)bool male;
 
